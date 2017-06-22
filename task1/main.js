90c496a8dc24c9ec3318f8b35246f25f8e1f94db
Anatolii Lakhno <tolikrss@gmail.com>
2017-06-22 10:37:44 +0300

    added method "clearAll"

--- task1/main.js
+++ task1/main.js
@@ -49,16 +49,6 @@ var childComponent1 = {
             console.log('model1 expirience chanched');
             console.log(this.expirience);
         },
-        languages: function() {
-            this.$emit('filter-changed', this.expirience, this.languages, this.educations);
-            console.log('model1 languages chanched');
-            console.log(this.languages);
-        },
-        educations: function() {
-            this.$emit('filter-changed', this.expirience, this.languages, this.educations);
-            console.log('model1 educations chanched');
-            console.log(this.educations);
-        }
     },
     methods: {
         clearAll() {
